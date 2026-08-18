271febac5cef21fee8650b0b4d7cb86b203e848f
SOFIA ALEXANDRINA CORNEA PERALTA <26171635@edu119.local>
2026-08-18 09:40:25 -0300

    salvar

---
+++ 01-funcoes/02-parametros.js
@@ -0,0 +1,17 @@
+//  function apresentar(nome) {
+//      console.log(`Olá, ${nome}`);
+//  }
+
+//  apresentar("Sofia");
+
+function somar(a ,b){
+    console.log(`${a} + ${b} = ${a+b}`);
+}
+
+somar (8, 15);
+
+function apresentar(nome) {
+     console.log(`Olá, ${nome}`);
+ }
+
+ apresentar("Sofia");
